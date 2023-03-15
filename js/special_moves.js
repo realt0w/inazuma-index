887941f36643da0afdae102e184059856b462131
Tom ツ <emodzz@yahoo.com>
2023-03-15 02:04:50 +0100

    Added separate super moves file.
    Created special_moves.js
    Moved js & css files into folders

---
+++ js/special_moves.js
@@ -0,0 +1,148 @@
+const special_moves = {
+    1: {
+        name: "God Hand", // IE 1
+        TP: 21,
+        element: "Earth",
+        type: "Catch",
+    },
+    2: {
+        name: "God Hand", // IE 1 (Blue), Element change.
+        TP: 21,
+        element: "Wood",
+        type: "Catch",
+    },
+    3: {
+        name: "Grenade Shot", // IE 1
+        TP: 17,
+        element: "Fire",
+        type: "Shoot",
+    },
+    4: {
+        name: "Majin The Hand", // IE 1
+        TP: 46,
+        element: "Earth",
+        type: "Catch",
+    },
+    5: {
+        name: "Triple Defense", // IE 1
+        TP: 46,
+        element: "Earth",
+        type: "Catch",
+    },
+    6: {
+        name: "The Wall", // IE 1
+        TP: 25,
+        element: "Earth",
+        type: "Block",
+    },
+    7: {
+        name: "Super Armadillo", // IE 1
+        TP: 22,
+        element: "Earth",
+        type: "Dribble",
+    },
+    8: {
+        name: "Mole Fake", // IE 1
+        TP: 30,
+        element: "Earth",
+        type: "Dribble",
+    },
+    9: {
+        name: "Mega Quake", // IE 1
+        TP: 38,
+        element: "Earth",
+        type: "Block",
+    },
+    10: {
+        name: "Coil Turn", // IE 1
+        TP: 15,
+        element: "Wind",
+        type: "Block",
+    },
+    11: {
+        name: "About Face", // IE 1
+        TP: 17,
+        element: "Earth",
+        type: "Block",
+    },
+    12: {
+        name: "Afterimage", // IE 1
+        TP: 25,
+        element: "Wood",
+        type: "Dribble",
+    },
+    13: {
+        name: "Shadow Stitch", // IE 1
+        TP: 33,
+        element: "Wood",
+        type: "Block",
+    },
+    14: {
+        name: "Flurry Dash", // IE 1
+        TP: 15,
+        element: "Wind",
+        type: "Dribble",
+    },
+    15: {
+        name: "Quick Draw", // IE 1
+        TP: 15,
+        element: "Wood",
+        type: "Block",
+    },
+    16: {
+        name: "Fire Rooster", // IE 1
+        TP: 56,
+        element: "Fire",
+        type: "Shoot",
+    },
+    17: {
+        name: "Clone Faker", // IE 1
+        TP: 36,
+        element: "Wood",
+        type: "Dribble",
+    },
+    18: {
+        name: "Rodeo Clown", // IE 1
+        TP: 15,
+        element: "Wind",
+        type: "Dribble",
+    },
+    19: {
+        name: "Comet Shot", // IE 1
+        TP: 24,
+        element: "Wind",
+        type: "Shoot",
+    },
+    20: {
+        name: "Monkey Turn", // IE 1
+        TP: 17,
+        element: "Earth",
+        type: "Dribble",
+    },
+    21: {
+        name: "Run Ball Run", // IE 1
+        TP: 33,
+        element: "Wood",
+        type: "Shoot",
+    },
+    22: {
+        name: "Whirlwind Twister", // IE 1
+        TP: 17,
+        element: "Wind",
+        type: "Dribble",
+    },
+    23: {
+        name: "Kung Fu Header", // IE 1
+        TP: 35,
+        element: "Wood",
+        type: "Shoot",
+    },
+    24: {
+        name: "Clone Shot", // IE 1
+        TP: 51,
+        element: "Wood",
+        type: "Shoot",
+    },
+    // Add more special moves here
+};
+
